9bf19bd9e3374f22abbd9cd5a2b079cd02d28810
Angelika <watroba.angelika@gmail.com>
2021-06-02 10:59:02 +0200

    Product not found exception handler

--- src/main/java/com/awatroba/shop/controllers/ProductsController.java
+++ src/main/java/com/awatroba/shop/controllers/ProductsController.java
@@ -1,73 +1,56 @@
 package com.awatroba.shop.controllers;
 
 import com.awatroba.shop.exception.ProductNotFoundException;
-import com.awatroba.shop.helpers.CreateProductRequest;
-import com.awatroba.shop.helpers.Response;
+import com.awatroba.shop.helpers.CreateUserRequest;
 import com.awatroba.shop.models.Product;
 import com.awatroba.shop.services.ProductsService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-/**
- * @author Angelika
- * Dashboard request controller
- */
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.servlet.ModelAndView;
 
-@RestController()
-@RequestMapping("/dashboard")
+@Controller
 public class ProductsController {
+    private ModelAndView model;
+    private ProductsService productsService;
+
+    private static String CREATE_USER_REQUEST = "userRequest";
+    private static String MESSAGE_ERROR = "messageError";
+    private static String MESSAGE_SUCCESS = "messageSuccess";
+    private static String PRODUCTS_PARAM = "products";
+    private static String PRODUCT_PARAM = "product";
+    private static String DASHBOARD_MODEL_NAME = "dashboard";
+    private static String PROD_DET_MODEL_NAME = "showProduct";
+
     @Autowired
-    ProductsService productsService;
-
-    @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity<Object>  getAllProducts() {
-        Response<Iterable<Product>> response =
-                new Response<Iterable<Product>>("success",  productsService.getAllProducts());
-        return new ResponseEntity<Object>(response, HttpStatus.OK);
+    public ProductsController(ProductsService productsService) {
+        this.productsService = productsService;
+        model = new ModelAndView(DASHBOARD_MODEL_NAME);
+        model.addObject(CREATE_USER_REQUEST, new CreateUserRequest());
+        model.addObject(MESSAGE_ERROR, "");
+        model.addObject(MESSAGE_SUCCESS, "");
     }
-    @RequestMapping(value = "/{category}",method = RequestMethod.GET)
-    public ResponseEntity<Object> getAllProductsByCategory(@PathVariable String category) {
-        Response<Iterable<Product>> response =
-                new Response<Iterable<Product>>("success",  productsService.getAllProductsByCategory(category));
-        return new ResponseEntity<Object>(response, HttpStatus.OK);
+    @GetMapping("/dashboard")
+    public ModelAndView getDashboard() {
+        model.setViewName(DASHBOARD_MODEL_NAME);
+        model.addObject(PRODUCTS_PARAM, productsService.getAllProducts());
+        model.addObject(MESSAGE_ERROR, "");
+        model.addObject(MESSAGE_SUCCESS, "");
+        return model;
     }
 
-    @RequestMapping(value = "/{bookId}", method = RequestMethod.GET)
-    public ResponseEntity<Object> getProductById(@PathVariable Long id) {
-        Response<Product> response;
-        try{
-            response =  new Response<Product>("success",  productsService.getProductById(id));
-            return new ResponseEntity<Object>(response, HttpStatus.OK);
+    @GetMapping("/dashboard/{id}")
+    public ModelAndView getProductById(@PathVariable("id") Long id) {
+        model.setViewName(PROD_DET_MODEL_NAME);
+        model.addObject(MESSAGE_ERROR, "");
+        model.addObject(MESSAGE_SUCCESS, "");
+        try {
+            model.addObject(PRODUCT_PARAM, productsService.getProductById(id));
         }catch (ProductNotFoundException e){
-            response =  new Response<Product>("error",  new Product());
-            return new ResponseEntity<Object>(response, HttpStatus.INTERNAL_SERVER_ERROR);
+            model.addObject(MESSAGE_ERROR, e.getMessage());
+            model.addObject(PRODUCT_PARAM, new Product());
         }
-    }
-
-    @RequestMapping(method = RequestMethod.POST)
-    public ResponseEntity<Object> addNewProduct(@RequestBody CreateProductRequest createProductRequest) {
-        String errorMessage =productsService.addNewProduct(createProductRequest);
-        if(errorMessage.equals("")){
-            Response<CreateProductRequest>  response =
-                    new Response<CreateProductRequest>("success", createProductRequest);
-            return new ResponseEntity<Object>(response, HttpStatus.OK);
-        } else {
-            Response<String> response =
-                    new Response<String>("error", errorMessage);
-            return new ResponseEntity<Object>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
-    }
-
-    @RequestMapping(value = "/{productId}", method = RequestMethod.DELETE)
-    public ResponseEntity<Object> deleteBook(@PathVariable Long productId) {
-        Response<Product> response;
-        try{
-            response =  new Response<Product>("success",  productsService.deleteProduct(productId));
-            return new ResponseEntity<Object>(response, HttpStatus.OK);
-        }catch (ProductNotFoundException e){
-            response =  new Response<Product>("error",  new Product());
-            return new ResponseEntity<Object>(response, HttpStatus.INTERNAL_SERVER_ERROR);
-        }
+        return model;
     }
 }
